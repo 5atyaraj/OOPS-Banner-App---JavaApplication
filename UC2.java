f0b507b94cca376b03e5d5c3885b31f836fc90bf
SATYARAJ <sn2935@srmist.edu.in>
2026-02-23 14:10:59 +0530

    first commit

---
+++ UC2.java
@@ -0,0 +1,15 @@
+public class UC1 {
+    public static void main(String[] args) {
+
+        // OOPS - Object Oriented Programming System
+
+        System.out.println("  *****  " + "   *****  " + "   *****  " + "   *****  ");
+        System.out.println(" *     * " + "  *     * " + "  *     * " + "  *     * ");
+        System.out.println(" *     * " + "  *     * " + "  *     * " + "  *       ");
+        System.out.println(" *     * " + "  *     * " + "  ******  " + "   *****  ");
+        System.out.println(" *     * " + "  *     * " + "  *       " + "        * ");
+        System.out.println(" *     * " + "  *     * " + "  *       " + "  *     * ");
+        System.out.println("  *****  " + "   *****  " + "  *       " + "   *****  ");
+
+    }
+}
